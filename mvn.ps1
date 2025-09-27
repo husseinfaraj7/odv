9b7df5e4772ed8e0a0517103d282e570a0e7b8c8
husseinfaraj7 <ussofaraj@gmail.com>
2025-09-27 15:16:19 +0200

    Delete mvn.ps1

--- mvn.ps1
+++
@@ -1,8 +0,0 @@
-# Maven wrapper script using Docker
-param(
-    [Parameter(ValueFromRemainingArguments=$true)]
-    [string[]]$MavenArgs
-)
-
-$workDir = Get-Location
-docker run --rm -v "${workDir}:/app" -w /app/backend maven:3.9.6-openjdk-17 mvn @MavenArgs
